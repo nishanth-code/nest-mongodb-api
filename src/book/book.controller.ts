36c1602cf20e5090c17aec2aa0cfb1e82ce3d731
nishanth-code <nishanthnavneeth200@gmail.com>
2024-03-14 16:38:56 +0530

    final commit

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 
@@ -9,5 +9,25 @@ export class BookController {
     @Get()
     async getAllBooks(): Promise<Book[]>{
         return this.bookService.findAll()
+    }
+
+    @Post()
+    async createBook(@Body()book ):Promise<Book>{
+        return this.bookService.create(book)
+
+    }
+    @Get(':id')
+    async getbook(@Param('id')id:string): Promise<Book>{
+        return this.bookService.findOne(id)
+    }
+
+    @Put(":id")
+    async updatebook(@Param('id')id:string,@Body()book):Promise<Book>{
+        return this.bookService.updateByid(id,book)
+
+    }
+    @Delete(":id")
+    async deleteBook(@Param('id')id:string):Promise<Book>{
+        return this.bookService.delete(id)
     }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose'
 import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+import { updateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BookService {
@@ -13,5 +15,24 @@ export class BookService {
     async findAll():Promise<Book[]>{
         const books = await this.bookModel.find()
         return books
+    }
+    async create(book:CreateBookDto):Promise<Book>{
+        const res = await this.bookModel.create(book)
+        return res
+
+    }
+    async findOne(id:string):Promise<Book>{
+        const res = await this.bookModel.findById(id)
+        if (!res){
+            throw new NotFoundException()
+        }
+        return res
+
+    }
+    async updateByid(id:string,book:updateBookDto):Promise<Book>{
+        return await this.bookModel.findByIdAndUpdate(id,book,{new:true,runValidators:true})
+    }
+    async delete(id:string):Promise<Book>{
+        return await this.bookModel.findByIdAndDelete(id)
     }
 }
